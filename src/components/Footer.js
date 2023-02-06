1dc41cc9df5e45dcdb2d3f09d70331c764dd06c5
marlborocool <greatrj890106@gmail.com>
2023-02-06 15:51:21 +0900

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import {
+  AiFillGithub,
+  AiFillSkype,
+} from "react-icons/ai";
+import { FaDiscord, FaTelegram } from "react-icons/fa";
+
+function Footer() {
+  let date = new Date();
+  let year = date.getFullYear();
+  return (
+    <Container fluid className="footer">
+      <Row>
+        <Col md="4" className="footer-copywright">
+          <h3>Designed and Developed by Layton Hart</h3>
+        </Col>
+        <Col md="4" className="footer-copywright">
+          <h3>Copyright © {year} LH</h3>
+        </Col>
+        <Col md="4" className="footer-body">
+          <ul className="footer-icons">
+            <li className="social-icons">
+              <a
+                href="https://github.com/marlborocool"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <AiFillGithub />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://t.me/marlborocool"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <FaTelegram />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://discord.gg/DPpBM5U6"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <FaDiscord />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://join.skype.com/invite/uVJfyGmxJ4z8"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <AiFillSkype />
+              </a>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Footer;
